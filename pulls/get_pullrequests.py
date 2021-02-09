ee2c7a0551e4031f50b4e1420249adc2a15277b8
pamsn <pauloadmsn@gmail.com>
2021-02-09 19:53:29 -0300

    resolvendo o problema dos pull requests

--- pulls/get_pullrequests.py
+++ pulls/get_pullrequests.py
@@ -12,9 +12,7 @@ headers = {'user-agent': 'pamsn'}
 
 repositories = [
                 # 'https://github.com/ArduPilot/ardupilot',
-                'https://github.com/betaflight/betaflight',
-                'https://github.com/PX4/PX4-Autopilot',
-                'https://github.com/cleanflight/cleanflight',
+                # 'https://github.com/PX4/PX4-Autopilot',
                 'https://github.com/iNavFlight/inav',
                 'https://github.com/paparazzi/paparazzi']
 
@@ -27,22 +25,24 @@ def pulls_of_repo_github(owner, repo, api):
     pulls = []
     next = True
     i = 1
-    total_requests = 0
-    remaining = get_rate_limit(github_api)
+    # total_requests = 0
+    # remaining = get_rate_limit(github_api)
     while next == True:
         # total_requests = total_requests + 1
         # if total_requests > remaining:
         #     time.sleep(60 * 60)
         #     print("Sleeping...")
         #     total_requests = 0
-        url = api + '/repos/{}/{}/pulls?state=all?page={}&per_page=100&access_token={}'.format(owner, repo, i, "a9aac7a15552b60618b40320d2d7261a4e232c2b")
+        url = api + '/repos/{}/{}/pulls?state=closed&page={}&per_page=100&access_token={}'.format(owner, repo, i, "a9aac7a15552b60618b40320d2d7261a4e232c2b")
         time.sleep(1)
         print(url)
         pull_pg = gh_session.get(url = url, headers = headers)
         pull_pg_list = [dict(item, **{'repo_name':'{}'.format(repo)}) for item in pull_pg.json()]
         pull_pg_list = [dict(item, **{'owner':'{}'.format(owner)}) for item in pull_pg_list]
         pulls = pulls + pull_pg_list
+        # print(pull_pg.headers)
         if 'Link' in pull_pg.headers:
+            # print(pull_pg.headers['Link'])
             if 'rel="next"' not in pull_pg.headers['Link']:
                 next = False
         i = i + 1
@@ -50,7 +50,7 @@ def pulls_of_repo_github(owner, repo, api):
 
 def create_pulls_df(owner,repo, api):
     pulls_list = pulls_of_repo_github(owner, repo, api)
-    return pandas.json_normalize(pulls_list)
+    return pandas.json_normalize(pulls_list )
 
 
 i = 0
@@ -60,7 +60,7 @@ for url in repositories:
     repo = url.split("/")[4].replace(".git", "")
     df = create_pulls_df(owner,repo, github_api)
     import json
-    with open('pulls/' + repo + '.json', 'w', encoding='utf-8') as f:
+    with open('/Users/pamsn/Projects/drone_project/githubapi/pulls/' + repo + '.json', 'w', encoding='utf-8') as f:
         df = df.to_json()
         json.dump(df, f, ensure_ascii=False, indent=4)
     time.sleep(300)
--- pulls/pull_analuzer.py
+++ pulls/pull_analuzer.py
@@ -4,12 +4,10 @@ import pandas as pd
 
 # df = pd.read_json('/Users/belize/projetos_github/AnaDrones/githubapi/ardupilot.json')
 
-files = ['/Users/pamsn/Projects/drone_project/codigo_drone/pulls/ardupilot.json',
-         '/Users/pamsn/Projects/drone_project/codigo_drone/pulls/betaflight.json',
-         '/Users/pamsn/Projects/drone_project/codigo_drone/pulls/PX4-Autopilot.json',
-         '/Users/pamsn/Projects/drone_project/codigo_drone/pulls/cleanflight.json',
-         '/Users/pamsn/Projects/drone_project/codigo_drone/pulls/inav.json',
-         '/Users/pamsn/Projects/drone_project/codigo_drone/pulls/paparazzi.json',
+files = [
+
+         '/Users/pamsn/Projects/drone_project/githubapi/pulls/inav.json',
+         # '/Users/pamsn/Projects/drone_project/codigo_drone/pulls/paparazzi.json',
          ]
 
 for file in files:
@@ -26,5 +24,5 @@ for file in files:
 
     # is_closed = df.loc[df['state'] == 'closed']
 
-    commits = df[df['commit.message'].str.contains('fix|defect|error|bug|issue|mistake|incorrect|fault|flaw')]
+    commits = df[df['title'].str.contains('fix|defect|error|bug|issue|mistake|incorrect|fault|flaw')]
     print("Project Name:" + file +"   "+str(len(commits)) + "   Total Commits: " + str(len(df.index)))
